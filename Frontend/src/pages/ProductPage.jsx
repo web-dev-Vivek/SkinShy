0ba0fa668c249bea564009170b03bc3ef957b1dd
Vivek Jha <progamervivek2020@gmail.com>
2026-04-26 15:25:54 +0530

    .

--- Frontend/src/pages/ProductPage.jsx
+++ Frontend/src/pages/ProductPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { useParams, useNavigate} from 'react-router-dom';
 import { useAuth } from '@clerk/clerk-react';
 import { useOnboarding } from '../context/OnboardingContext';
 import OnboardingWarningBanner from '../components/Common/OnboardingWarningBanner';
