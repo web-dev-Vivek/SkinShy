55036108c25bb76c6d39313bff2df67966e0ab80
Vivek Jha <progamervivek2020@gmail.com>
2026-06-11 15:40:27 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useAuth } from '@clerk/clerk-react';
@@ -7,16 +7,19 @@ import { UserProvider } from './context/UserContext';
 import { CurrencyProvider } from './context/CurrencyContext';
 import { setClerkTokenGetter } from './services/api';
 import Navbar from './components/Common/Navbar';
-import LandingPage from './pages/LandingPage';
-import SearchPage from './pages/SearchPage';
-import ProductPage from './pages/ProductPage';
-import ProfilePage from './pages/ProfilePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import OnboardingPage from './pages/OnboardingPage';
-import ProductComparePage from './pages/ProductComparePage';
-import GuidePage from './pages/GuidePage';
+import LoadingFallback from './components/LoadingFallback';
 import ProtectedRoute from './components/ProtectedRoute';
+
+// Lazy load all pages for code splitting
+const LandingPage = React.lazy(() => import('./pages/LandingPage'));
+const SearchPage = React.lazy(() => import('./pages/SearchPage'));
+const ProductPage = React.lazy(() => import('./pages/ProductPage'));
+const ProfilePage = React.lazy(() => import('./pages/ProfilePage'));
+const LoginPage = React.lazy(() => import('./pages/LoginPage'));
+const SignupPage = React.lazy(() => import('./pages/SignupPage'));
+const OnboardingPage = React.lazy(() => import('./pages/OnboardingPage'));
+const ProductComparePage = React.lazy(() => import('./pages/ProductComparePage'));
+const GuidePage = React.lazy(() => import('./pages/GuidePage'));
 
 // Component to initialize token getter - must be inside Router
 function TokenInitializer() {
@@ -39,55 +42,57 @@ function AppRoutes() {
     <>
       <TokenInitializer />
       {shouldShowNavbar && <Navbar />}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/onboarding"
-          element={
-            <ProtectedRoute>
-              <OnboardingPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/search"
-          element={
-            <ProtectedRoute>
-              <SearchPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/search/:id"
-          element={
-            <ProtectedRoute>
-              <ProductPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <ProfilePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/product_Comparasion"
-          element={
-            <ProtectedRoute>
-              <ProductComparePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/guide"
-          element={<GuidePage />}
-        />
-      </Routes>
+      <Suspense fallback={<LoadingFallback />}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/onboarding"
+            element={
+              <ProtectedRoute>
+                <OnboardingPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute>
+                <SearchPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/search/:id"
+            element={
+              <ProtectedRoute>
+                <ProductPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/product_Comparasion"
+            element={
+              <ProtectedRoute>
+                <ProductComparePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/guide"
+            element={<GuidePage />}
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 }
---
+++ Frontend/src/components/LoadingFallback.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PageSkeleton from './Skeletons/PageSkeleton';
+
+export default function LoadingFallback() {
+  return <PageSkeleton />;
+}
